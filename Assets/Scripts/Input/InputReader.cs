9b02c6253db8c7bd47e85f1b292e41b358f42c02
Cornetto <lior5858@gmail.com>
2025-05-02 00:32:39 +0300

    Start setting up input

---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using static PlayerInputActions;
+[CreateAssetMenu(fileName = "InputReader", menuName = "Input/InputReader")]
+public class InputReader : ScriptableObject, IPlayerActions
+{
+    public event UnityAction<Vector2> Move = delegate { };
+    public event UnityAction<Vector2, bool> Look = delegate { };
+    public event UnityAction<bool> Jump = delegate { };
+    public event UnityAction<bool> Dash = delegate { };
+    public event UnityAction Attack = delegate { };
+
+    PlayerInputActions inputActions;
+
+    public Vector3 Direction => inputActions.Player.Move.ReadValue<Vector2>();
+
+    void OnEnable() {
+        if (inputActions == null) {
+            inputActions = new PlayerInputActions();
+            inputActions.Player.SetCallbacks(this);
+        }
+        EnablePlayerActions();
+    }
+    
+    public void EnablePlayerActions() {
+        inputActions.Enable();
+    }
+    
+
+    bool IsDeviceMouse(InputAction.CallbackContext context) => context.control.device.name == "Mouse";
+
+    public void OnMove(InputAction.CallbackContext context) {
+        Move.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnLook(InputAction.CallbackContext context) {
+        Look.Invoke(context.ReadValue<Vector2>(), IsDeviceMouse(context));
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started) {
+            Attack.Invoke();
+        }
+    }
+
+    public void OnNext(InputAction.CallbackContext context)
+    {
+        //noop
+    }
+
+    public void OnPrevious(InputAction.CallbackContext context)
+    {
+        //noop
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        //noop
+    }
+
+    
+
+    public void OnCrouch(InputAction.CallbackContext context)
+    {
+        //noop
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        //noop
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        //noop
+    }
+}
