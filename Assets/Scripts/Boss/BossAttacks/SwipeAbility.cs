a0faa9e0cdd531f759a49c271e0a6ebc83236b28
Cornetto <lior5858@gmail.com>
2025-06-29 18:59:12 +0300

    Sword ability movement

--- Assets/Scripts/Boss/BossAttacks/SwipeAbility.cs
+++ Assets/Scripts/Boss/BossAttacks/SwipeAbility.cs
@@ -1,20 +1,26 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Boss.BossAttacks
 {
     public class SwipeAbility : BossAbility
     {
+        [SerializeField] private Transform visuals;
         [SerializeField] private GameObject swipeObject;
         [SerializeField] private Transform startPoint;
         [SerializeField] private Transform endPoint;
+        [SerializeField] private AnimationCurve curve;
         
         public override void Activate()
         {
             base.Activate();
             swipeObject.transform.position = startPoint.position;
+            visuals.rotation = Quaternion.Euler(0, 0, 90);
             swipeObject.SetActive(true);
-            swipeObject.transform.DOJump(endPoint.position, 1, 1, abilityData.Duration);
+            Vector3 controlPoint = (endPoint.position + startPoint.position) / 2 + transform.forward * 4;
+            swipeObject.transform.DOPath(new Vector3[]{startPoint.position, controlPoint, endPoint.position }, abilityData.Duration, PathType.CatmullRom, gizmoColor: Color.red).SetEase(curve);
+            visuals.DORotate(new Vector3(0, 180, visuals.transform.rotation.eulerAngles.z), abilityData.Duration).SetEase(curve);
         }
     }
 }
